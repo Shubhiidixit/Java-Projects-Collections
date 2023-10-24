4be6e95d63cac1b48e20916aad3db6b2eb461e7b
Shubhi Dixit <shubhidixit2903@gmail.com>
2023-10-24 16:51:55 +0530

    Create Simple_Interest.java

---
+++ Simple_Interest.java
@@ -0,0 +1,10 @@
+import java.io.*;
+
+class Shubhi {
+	public static void main(String args[])
+	{
+		float P = 1, R = 1, T = 1;
+		float SI = (P * T * R) / 100;
+		System.out.println("Simple_Interest = " + SI);
+	}
+}
